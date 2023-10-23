b7e1e3d2f3648fd4fe313e261c19333fd75112cd
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-23 17:07:12 +0100

    task 3 is done.

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format.
+"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    # inti all variables:
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+    # loop over users and save data:
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
